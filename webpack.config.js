19cc33cdb68313350f7d69f53cf5cc8d154e7d94
Faustino Martinez <faustmtz@protonmail.com>
2018-06-27 17:57:06 -0700

    Webpack.config.js updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-	entry: '',
+	entry: path.resolve(__dirname, 'src') + '/app/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist') + '/app',
+		publicPath: '/app/'
 	},
 	module: {
 		rules: [
